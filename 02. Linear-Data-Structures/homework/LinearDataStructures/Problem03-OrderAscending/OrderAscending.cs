bfae5eafa1f0f2ecdae5565fc48ab35d197c010c
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-11-01 19:42:39 +0200

    Added solutions to problems 1, 2, 3

---
+++ 02. Linear-Data-Structures/homework/LinearDataStructures/Problem03-OrderAscending/OrderAscending.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem03_OrderAscending
+{
+    using System;
+    using System.Collections.Generic;
+
+    class OrderAscending
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads a sequence of integers(List<int>) ending with an empty line
+            //and sorts them in an increasing order.
+
+            var inputLine = Console.ReadLine();
+            List<int> numbers = new List<int>();
+
+            while (!string.IsNullOrEmpty(inputLine))
+            {
+                numbers.Add(int.Parse(inputLine));
+
+                inputLine = Console.ReadLine();
+            }
+
+            numbers.Sort();
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ 02. Linear-Data-Structures/homework/LinearDataStructures/Problem2-ReverseIntegers/ReverseIntegers.cs
@@ -0,0 +1,24 @@
+﻿namespace Problem2_ReverseIntegers
+{
+    using System;
+    using System.Collections.Generic;
+
+    class ReverseIntegers
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program that reads N integers from the console and reverses them using a stack.
+            Use the Stack < int > class.*/
+
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Push(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
