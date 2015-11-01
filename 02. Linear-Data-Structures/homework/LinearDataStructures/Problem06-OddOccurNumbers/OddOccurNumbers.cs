96a5067fd5cd03b81a6758919802188109c32a6f
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-11-01 21:31:52 +0200

    added solution to problem 6

---
+++ 02. Linear-Data-Structures/homework/LinearDataStructures/Problem06-OddOccurNumbers/OddOccurNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem06_OddOccurNumbers
+{
+    class OddOccurNumbers
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that removes from given sequence all numbers that occur odd number of times.
+
+            List<int> numbers = new List<int> {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2};
+
+            Dictionary<int, int> occurencies = new Dictionary<int, int>();
+            GetOccurenciesForEachNumber(numbers, occurencies);
+            RemoveNumbersWithOddOccurencies(occurencies, numbers);
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static void RemoveNumbersWithOddOccurencies(Dictionary<int, int> occurencies, List<int> numbers)
+        {
+            foreach (var pair in occurencies)
+            {
+                if (pair.Value%2 != 0)
+                {
+                    bool areAnyNumbersLeft = true;
+                    while (areAnyNumbersLeft)
+                    {
+                        areAnyNumbersLeft = numbers.Remove(pair.Key);
+                    }
+                }
+            }
+        }
+
+        private static void GetOccurenciesForEachNumber(List<int> numbers, Dictionary<int, int> occurencies)
+        {
+            foreach (int number in numbers)
+            {
+                if (occurencies.ContainsKey(number))
+                {
+                    occurencies[number]++;
+                }
+                else
+                {
+                    occurencies.Add(number, 1);
+                }
+            }
+        }
+    }
+}
