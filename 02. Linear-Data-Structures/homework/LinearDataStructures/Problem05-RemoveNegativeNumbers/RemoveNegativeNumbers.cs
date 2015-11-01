131f39ddf3b6b5ed11c66f0108759d872a3a7e77
YanaSlavcheva <yana.slavcheva@gmail.com>
2015-11-01 21:09:25 +0200

    added solution to problem 5

---
+++ 02. Linear-Data-Structures/homework/LinearDataStructures/Problem05-RemoveNegativeNumbers/RemoveNegativeNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem05_RemoveNegativeNumbers
+{
+    class RemoveNegativeNumbers
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that removes from given sequence all negative numbers.
+
+            List<int> numbers = new List<int> {1, 2, -4, 5, -7, -18, 23};
+            numbers = numbers.Where(x => x >= 0).ToList();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
